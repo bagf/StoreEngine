e4fd5aa9bb681f0568f294e38844a9ac53b6e7b6
bagf <rory@eezipay.com>
2013-11-28 23:27:02 +0200

    Changed StoreEngine to add paths via a static method (to make use of late static binding)

--- objects/StoreEngine.class.php
+++ objects/StoreEngine.class.php
@@ -6,7 +6,13 @@
 namespace StoreEngine;
 
 class StoreEngine {
-     
+    
+    protected static $paths = array();
+
+    protected static function addClassPath($class, $path) {
+        static::$paths[] = array("class" => $class, "path" => $path);
+    }
+
     private static function findClass($class_name, $directory) {
         $di = new \DirectoryIterator($directory);
         foreach($di as $d) {
@@ -24,13 +30,6 @@ class StoreEngine {
     }
 
     public static function autoload($class_name) {
-        $class_name = str_replace(__NAMESPACE__."\\", '', $class_name);
-        $paths = array();
-        $paths[] = array("class" => "{$class_name}.class", "path" => __DIR__."/");
-        $paths[] = array("class" => "{$class_name}.class", "path" => __DIR__."/factories/");
-        $paths[] = array("class" => "{$class_name}.interface", "path" => __DIR__."/interfaces/");
-        $paths[] = array("class" => "{$class_name}.trait", "path" => __DIR__."/traits/");
-
         foreach($paths as $path) {
             $result = static::findClass($path['class'], $path['path']);
             if ($result !== false) {
@@ -43,6 +42,11 @@ class StoreEngine {
     }
 
     public static function registerAutoloader() {
+        $class_name = str_replace(__NAMESPACE__."\\", '', $class_name);
+        static::addClassPath("{$class_name}.class", __DIR__."/");
+        static::addClassPath("{$class_name}.class", __DIR__."/factories/");
+        static::addClassPath("{$class_name}.interface", __DIR__."/interfaces/");
+        static::addClassPath("{$class_name}.trait", __DIR__."/traits/");
         spl_autoload_register(__CLASS__ ."::autoload");
     }
 }
