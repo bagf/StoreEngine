9ad910f8447b1757519a5a779b9502a9b0366523
bagf <rory@eezipay.com>
2013-12-04 02:27:38 +0200

    Updated test scripts

--- tests/basic/init.php
+++ tests/basic/init.php
@@ -3,28 +3,13 @@
  * @author bagf
 */
 namespace StoreEngine;
-
-error_reporting(E_ALL | E_STRICT);
-
-if (!isset($lib_storm, $db_host, $db_username, $db_password, $db_name, $db_port)) {
-    throw new \Exception("Missing variables cannot init unit test");
-}
-
-// Relative paths
-$lib_storm = __DIR__.$lib_storm;
-
+require_once __DIR__.'/../settingsInit.php';
 // PHPUnit
-$lib_phpunit = "PHPUnit/autoload.php";
-
-if (!is_file($lib_storm)) {
-    throw new \Exception("Cannot find Storm development library");
-}
+$lib_phpunit = "PHPUnit/Autoload.php";
 
 if (!is_file($lib_phpunit)) {
     throw new \Exception("Cannot find PHPUnit library");
 }
 
-require_once $lib_storm;
-require_once __DIR__ .'/../../autoload.php';
 require_once $lib_phpunit;
 
